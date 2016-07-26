c572f6ded4c1f39a03e852e663a2bc8298d610cc
Lee Marsh <npnlee85@gmail.com>
2016-07-26 10:37:41 -0700

    Adding ancillary dataset sheets, making code DRY, some text changes

--- makeMetadata.php
+++ makeMetadata.php
@@ -30,7 +30,19 @@ define('RAW_TYPE', 'raw');
 define("INDIVIDUAL_TYPE", 'individual_summarized');
 define("SITE_TYPE", 'site_summarized');
 
-
+$ancilliary_types = array(
+    'ancillary_dataset' => 'dataset', 
+    'ancillary_person' => 'person', 
+    'ancillary_site' => 'station', 
+    'ancillary_individual_plant' => 'plant', 
+    'ancillary_protocol' => 'protocol', 
+    'ancillary_species_protocol' => 'species_protocol', 
+    'ancillary_phenophase' => 'phenophase', 
+    'ancillary_phenophase_def' => 'phenophase_definition', 
+    'ancillary_spp-specific' => 'sspi', 
+    'ancillary_intensity' => 'intensity', 
+    'ancillary_obs_group' => 'observation_group'
+    );
 
 include 'Classes/PHPExcel.php';
 PHPExcel_Settings::setZipClass(PHPExcel_Settings::PCLZIP);
@@ -41,41 +53,59 @@ include 'Classes/PHPExcel/Writer/Excel2007.php';
  * Setup all the various workbooks to generate, including the composite
  * book which will contain all the sheets added to the other books.
  */
-$book_composite = createWorkbook("Some Title", "Some Subject", "Some Description");
+$book_composite = createWorkbook("Data Field Metadata for Raw and Summarized Observation Data", "", "");
 $book_composite->createSheet();
 $book_composite->createSheet();
 
-$book_raw = createWorkbook("Some Title", "Some Subject", "Some Description");
-$book_individual_summarize = createWorkbook("Some Title", "Some Subject", "Some Description");
-$book_site_summarize = createWorkbook("Some Title", "Some Subject", "Some Description");
+$book_raw = createWorkbook("Data Field Metadata for Raw Status Observation Data", "", "");
+$book_individual_summarize = createWorkbook("Data Field Metadata for Individual-level Summarized Observation Data", "", "");
+$book_site_summarize = createWorkbook("Data Field Metadata for Site-level Summarized Observation Data", "", "");
 
-addRawSheet($book_composite, 0);
-addRawSheet($book_raw, 0);
+$book_ancilliary = createWorkbook("Data Field Metadata for Dataset Data", "", "");
 
-addIndividualSummarizedSheet($book_composite, 1);
-addIndividualSummarizedSheet($book_individual_summarize, 0);
 
-addSiteSummarizedSheet($book_composite, 2);
-addSiteSummarizedSheet($book_site_summarize, 0);
+addSheet($book_composite, 0, 'Raw', RAW_TYPE);
+addSheet($book_raw, 0, 'Raw', RAW_TYPE);
+
+addSheet($book_composite, 1, 'Individual-Summarized', INDIVIDUAL_TYPE);
+addSheet($book_individual_summarize, 0, 'Individual-Summarized', INDIVIDUAL_TYPE);
+
+
+addSheet($book_composite, 2, 'Site-Summarized', SITE_TYPE);
+addSheet($book_site_summarize, 0, 'Site-Summarized', SITE_TYPE);
+
+$num_ancilliary_sheets = 0;
+
+foreach($ancilliary_types as $name => $type){
+    if($num_ancilliary_sheets > 0){
+        $book_ancilliary->createSheet();
+    }
+    addSheet($book_ancilliary, $num_ancilliary_sheets++, $name, $type);    
+}
+
 
 
 //Reset the composite metadata sheet to the first page.
 $book_composite->setActiveSheetIndex(0);
+$book_ancilliary->setActiveSheetIndex(0);
 
 /**
  * Write all the files to disk
  */		
 $objWriter = new PHPExcel_Writer_Excel2007($book_composite);
-$objWriter->save(OUTPUT_PATH . 'composite_metadata.xlsx');
+$objWriter->save(OUTPUT_PATH . 'all_types_observation_metadata.xlsx');
 
 $objWriter = new PHPExcel_Writer_Excel2007($book_raw);
-$objWriter->save(OUTPUT_PATH . 'raw_metadata.xlsx');
+$objWriter->save(OUTPUT_PATH . 'raw_status_observation_metadata.xlsx');
 
 $objWriter = new PHPExcel_Writer_Excel2007($book_individual_summarize);
-$objWriter->save(OUTPUT_PATH . 'individual-summarized_metadata.xlsx');
+$objWriter->save(OUTPUT_PATH . 'individual-level_summarized_observation_metadata.xlsx');
 
 $objWriter = new PHPExcel_Writer_Excel2007($book_site_summarize);
-$objWriter->save(OUTPUT_PATH . 'site-summarized_metadata.xlsx');
+$objWriter->save(OUTPUT_PATH . 'site-level_summarized_observation_metadata.xlsx');
+
+$objWriter = new PHPExcel_Writer_Excel2007($book_ancilliary);
+$objWriter->save(OUTPUT_PATH . 'ancilliary_metadata.xlsx');
 
 
 /**
@@ -95,54 +125,13 @@ function createWorkbook($title, $subject, $description){
 }
 
 
-/**
- * This grabs the raw metadata type fields and adds them to a sheet.
- * @param PHPExcel $object : The workbook on which to operate
- * @param int $sheet_indx : The index of the work sheet for which to add to the workbook
- * This is necessary because for the composite book, we don't necessarily want to add the 
- * sheet to the beginning of the book.
- */
-function addRawSheet(&$object, $sheet_indx){
-    $object->setActiveSheetIndex($sheet_indx);
-    addHeaders($object);
-    $object->getActiveSheet()->setTitle('Raw');    
-    
-    fetchWriteData(RAW_TYPE, $object);
-
-}
-
-/**
- * This grabs the individual summarized  metadata type fields and adds them to a sheet.
- * @param PHPExcel $object : The workbook on which to operate
- * @param int $sheet_indx : The index of the work sheet for which to add to the workbook.
- * This is necessary because for the composite book, we don't necessarily want to add the 
- * sheet to the beginning of the book.
- */
-function addIndividualSummarizedSheet(&$object, $sheet_indx){
+function addSheet(&$object, $sheet_indx, $title, $type){
     $object->setActiveSheetIndex($sheet_indx);
     addHeaders($object);
     
-    $object->getActiveSheet()->setTitle('Individual-Summarized');
+    $object->getActiveSheet()->setTitle($title);
     
-    fetchWriteData(INDIVIDUAL_TYPE, $object);
-}
-
-
-/**
- * This grabs the site summarized metadata type fields and adds them to a sheet.
- * @param PHPExcel $object : The workbook on which to operate
- * @param int $sheet_indx : The index of the work sheet for which to add to the workbook.
- * This is necessary because for the composite book, we don't necessarily want to add the 
- * sheet to the beginning of the book.
- */
-function addSiteSummarizedSheet(&$object, $sheet_indx){
-    $object->setActiveSheetIndex($sheet_indx);
-    addHeaders($object);
-    
-    $object->getActiveSheet()->setTitle('Site-Summarized');
-    
-    fetchWriteData(SITE_TYPE, $object);
-    
+    fetchWriteData($type, $object);    
 }
 
 /**
